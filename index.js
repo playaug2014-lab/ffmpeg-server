79cb49f3c2acc37e6a8bebe2a49941072f067844
playaug2014-lab <playaug2014@gmail.com>
2026-04-23 17:34:27 +0530

    update
    done

--- index.js
+++ index.js
@@ -27,20 +27,26 @@ app.post('/render', upload.single('image'), async (req, res) => {
     fs.writeFileSync(audioPath, Buffer.from(audioRes.data));
     console.log('Audio downloaded!');
 
-    await new Promise((resolve, reject) => {
-      ffmpeg()
-        .input(imagePath)
-        .loop()
-        .input(audioPath)
-        .audioCodec('aac')
-        .videoCodec('libx264')
-        .size('1920x1080')
-        .outputOptions(['-pix_fmt yuv420p', '-shortest'])
-        .output(outputPath)
-        .on('end', () => { console.log('Done!'); resolve(); })
-        .on('error', (err) => { console.error('FFmpeg error:', err.message); reject(err); })
-        .run();
-    });
+  await new Promise((resolve, reject) => {
+  ffmpeg()
+    .input(imagePath)
+    .loop()
+    .input(audioPath)
+    .audioCodec('aac')
+    .videoCodec('libx264')
+    .size('1280x720')
+    .outputOptions([
+      '-pix_fmt yuv420p',
+      '-shortest',
+      '-preset ultrafast',
+      '-crf 28',
+      '-threads 1'
+    ])
+    .output(outputPath)
+    .on('end', () => { console.log('Done!'); resolve(); })
+    .on('error', (err) => { console.error('FFmpeg error:', err.message); reject(err); })
+    .run();
+});
 
     const video = fs.readFileSync(outputPath);
     res.set('Content-Type', 'video/mp4');
